f5c42cd4821f054328c81a48a6d1a8179d03e687
Steffen Stein <therealstein@tutanota.com>
2021-03-15 20:44:32 +0100

    added issueCreate

--- src/services/maintenance.service.js
+++ src/services/maintenance.service.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 import authHeader from './auth-header';
-const API_URL = process.env.VUE_APP_API_URL+'/api/maintenance/';
+const API_URL = process.env.VUE_APP_API_URL+'/api/user/';
 
 class MaintenanceService {
   sendReport(report) {
-    return axios.post(API_URL + 'sendreport',{
+    return axios.post(API_URL + 'create_issue',{
     "title": report.title,
     "label": report.label,
     "context": report.context,
