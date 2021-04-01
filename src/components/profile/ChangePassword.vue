df8a83c315d984eab75cfce96805a3ed9f3e91a6
Steffen Stein <therealstein@tutanota.com>
2021-04-01 14:09:06 +0200

    feat: change password in profile

---
+++ src/components/profile/ChangePassword.vue
@@ -0,0 +1,51 @@
+
+<template>
+  <div>
+    <div class="p-8">
+      <!-- TEXT -->
+      <FormulateForm
+        @submit="changePasswordNow"
+        v-model="password"
+        :schema="changePassword"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "change-password",
+  data: function () {
+    return {
+      password: {},
+      changePassword: [
+        {
+          label: "New password",
+          name: "password",
+          type: "password",
+          validation: "required",
+        },
+        {
+          label: "Confirm password",
+          type: "password",
+          name: "Passwords",
+          validation: "required|confirm",
+        },
+        {
+          type: "submit",
+          label: "Update Password",
+        },
+      ],
+    };
+  },
+  computed: {},
+  components: {},
+
+  methods: {
+    changePasswordNow() {
+      console.log(this.password.password);
+    },
+  },
+  mounted() {},
+};
+</script>
