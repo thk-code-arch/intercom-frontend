0c9de13854b1d16919901cd7fa73d3572e7dbceb
Steffen Stein <therealstein@tutanota.com>
2021-04-07 14:25:28 +0200

    fix: removed lazyLoad

--- src/router/index.js
+++ src/router/index.js
@@ -1,89 +1,100 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import Projects from '../views/Projects.vue';
-import Login from '../views/Login.vue';
-// import Register from '../views/Register.vue';
+import Vue from "vue";
+import Router from "vue-router";
+import Projects from "../views/Projects.vue";
+import Login from "../views/Login.vue";
+import TheView from "../views/TheView.vue";
+import Chat from "../views/Chat.vue";
+import Admin from "../views/Admin.vue";
+import Profile from "../views/Profile.vue";
+import ProjectSettings from "../views/ProjectSettings.vue";
+
+import Learning from "../views/Learning.vue";
+import ListLearning from "../components/learning/list-learnings.vue";
+import ShowLearning from "../components/learning/show-learning.vue";
+import AddLearning from "../components/learning/add-learning.vue";
 
 Vue.use(Router);
 
 export const router = new Router({
-  mode: 'history',
+  mode: "history",
   routes: [
     {
-      path: '/',
-      redirect: '/projects'
+      path: "/",
+      redirect: "/projects",
     },
     {
-      path: '/login',
+      path: "/login",
       component: Login,
     },
     {
-      path: '/view',
-      name: 'TheView',
-      // lazy-loaded
-      component: () => import('../views/TheView.vue')
+      path: "/view",
+      name: "TheView",
+      component: TheView,
     },
     {
-      path: '/chat',
-      name: 'Chat',
-      // lazy-loaded
-      component: () => import('../views/Chat.vue')
+      path: "/chat",
+      name: "Chat",
+      component: Chat,
     },
     {
-      path: '/learning',
-      name: 'Learning',
-      redirect: '/learning/list',
-      // lazy-loaded
-      component: () => import('../views/Learning.vue'),
-            children: [
-        { path: 'list', component: () => import('../components/learning/list-learnings.vue')},
-        { path: 'show/:id', component: () => import('../components/learning/show-learning.vue')},
-        { path: 'add', component: () => import('../components/learning/add-learning.vue')}
-      ]
+      path: "/learning",
+      name: "Learning",
+      redirect: "/learning/list",
+      component: Learning,
+      children: [
+        {
+          path: "list",
+          component: ListLearning,
+        },
+        {
+          path: "show/:id",
+          component: ShowLearning,
+        },
+        {
+          path: "add",
+          component: AddLearning,
+        },
+      ],
     },
     {
-      path: '/profile',
-      name: 'profile',
-      // lazy-loaded
-      component: () => import('../views/Profile.vue')
+      path: "/profile",
+      name: "profile",
+      component: Profile,
     },
     {
-      path: '/admin',
-      name: 'admin',
-      // lazy-loaded
-      component: () => import('../views/Admin.vue')
+      path: "/admin",
+      name: "admin",
+      component: Admin,
     },
     {
-      path: '/new-project',
-      name: 'new-project',
-      // lazy-loaded
-      component: () => import('../views/ProjectSettings.vue')
+      path: "/new-project",
+      name: "new-project",
+      component: ProjectSettings,
     },
     {
-      path: '/project-settings',
-      name: 'project-settings',
-      // lazy-loaded
-      component: () => import('../views/ProjectSettings.vue')
+      path: "/project-settings",
+      name: "project-settings",
+      component: ProjectSettings,
     },
     {
-      path: '/projects',
-      name: 'projects',
-      component: Projects
-    }
-  ]
+      path: "/projects",
+      name: "projects",
+      component: Projects,
+    },
+  ],
 });
 
 router.beforeEach((to, from, next) => {
-  const publicPages = ['/login'];
+  const publicPages = ["/login"];
   const authRequired = !publicPages.includes(to.path);
-  const loggedIn = localStorage.getItem('user');
+  const loggedIn = localStorage.getItem("user");
 
   // trying to access a restricted page + not logged in
   // redirect to login page
   if (authRequired && !loggedIn) {
-    next('/login');
+    next("/login");
   } else {
     next();
   }
 });
-export default router
+export default router;
