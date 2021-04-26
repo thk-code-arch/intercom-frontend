8097aa35fcc60e7cb97f64f6a8b3d67ec3088183
therealstein <therealstein@tutanota.com>
2021-04-26 08:09:02 +0200

    func: edit profile

---
+++ src/components/profile/ChangeProfileImage.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="flex justify-center px-2">
+    <div class="p-8 bg-white rounded-lg">
+      <!-- Top bar -->
+      <div class="flex items-center flex-none py-2 mb-4 border-b">
+        <div class="flex flex-col">
+          <h2 class="mb-2 text-2xl">Profile</h2>
+        </div>
+      </div>
+      <!-- TEXT -->
+      <div class="">
+        <img
+          :src="currentUser.profile_image"
+          class="object-cover w-full h-full"
+        />
+        <!-- TEXT -->
+        <label
+          class="flex flex-col inline-block px-3 py-1 mb-2 mr-2 font-semibold text-white bg-gray-800 rounded-full"
+        >
+          <input type="file" ref="file" @change="selectFile" hidden />
+        </label>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "change-profile-image",
+  data: function () {
+    return {
+      profile: {},
+      content: {},
+    };
+  },
+  computed: {
+    currentUser() {
+      return this.$store.state.auth.user;
+    },
+  },
+  components: {},
+
+  methods: {},
+  mounted() {
+    this.profile = this.$store.state.auth.user;
+  },
+};
+</script>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="container p-6 sm:px-8 flex flex-row">
-    <change-password />
     <update-profile />
+    <change-profile-image />
+    <change-password />
   </div>
 </template>
 
 <script>
 import ChangePassword from "../components/profile/ChangePassword";
 import UpdateProfile from "../components/profile/UpdateProfile";
+import ChangeProfileImage from "../components/profile/ChangeProfileImage";
 export default {
   name: "Profile",
-  components: { ChangePassword, UpdateProfile },
+  components: { ChangePassword, UpdateProfile, ChangeProfileImage },
 };
 </script>
