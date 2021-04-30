5b597a340a00d494164fbff910e33bdf16acf212
therealstein <therealstein@tutanota.com>
2021-04-30 13:59:35 +0000

    style: home edit

--- src/components/home/HelloUser.vue
+++ src/components/home/HelloUser.vue
@@ -1,28 +1,28 @@
 <template>
-    <div class="pt-4">
-        <h1 class="text-xl">hello {{currentUser.username}}!</h1>
-    </div>
+  <div class="pt-4 w-1/2 flex-row flex justify-around items-center my-6">
+    <h1 class="text-6xl italic tracking-wide text-gray-700">
+      hello {{ currentUser.username }}!
+    </h1>
+    <img
+      :src="currentUser.profile_image"
+      class="object-cover w-32 border-4 rounded-3xl border-codearch-500"
+    />
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloUser',
-  components: {
-      
-  },
+  name: "HelloUser",
+  components: {},
   computed: {
-
-          currentUser() {
+    currentUser() {
       return this.$store.state.auth.user;
     },
   },
-  mounted() {
+  mounted() {},
+  data() {
+    return {};
   },
- data(){
-  return {
-  }
- },
-  methods: {
-     }
+  methods: {},
 };
 </script>
