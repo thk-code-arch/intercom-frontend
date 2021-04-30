d0ae85b5da14d435c758b2c54e2bfeb96936e51b
Steffen Stein <31182980+therealstein@users.noreply.github.com>
2021-04-30 14:00:51 +0200

    Revert "styles: view,files"

--- src/views/TheView.vue
+++ src/views/TheView.vue
@@ -11,14 +11,9 @@
         <div class="flex flex-col justify-between w-6 h-full">
           <div class="flex"></div>
           <div
-            class="flex  cursor-pointer justify-center"
+            class="flex h-32 cursor-pointer bg-codearch-500"
             v-on:click="openSidebar('w-4/5', 'w-1/5')"
-          >
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-  <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm.707-10.293a1 1 0 00-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L9.414 11H13a1 1 0 100-2H9.414l1.293-1.293z" clip-rule="evenodd" />
-</svg>
-          
-          </div>
+          ></div>
           <div class="flex"></div>
         </div>
       </div>
