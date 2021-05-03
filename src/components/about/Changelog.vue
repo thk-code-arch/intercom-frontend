54dd9b06ab4fa41e0d28f96262f46f68ea582330
therealstein <therealstein@tutanota.com>
2021-05-03 10:27:16 +0000

    feat: about.vue

---
+++ src/components/about/Changelog.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="pt-4 flex-row flex justify-around items-center my-6">
+    <vue-markdown :source="changelog" />
+  </div>
+</template>
+
+<script>
+import VueMarkdown from "vue-markdown-render";
+export default {
+  name: "Changelog",
+  components: { VueMarkdown },
+  computed: {},
+  mounted() {
+    this.$http.get("changelog").then(
+      (response) => {
+        this.changelog = response.data;
+      },
+      (error) => {
+        this.content =
+          (error.response && error.response.data) ||
+          error.message ||
+          error.toString();
+      }
+    );
+  },
+  data() {
+    return { changelog: "changelog" };
+  },
+  methods: {},
+};
+</script>
---
+++ src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="w-full px-4 px-6 sm:px-8">
+    <div class="w-3/4 mx-auto">
+      <Changelog />
+    </div>
+  </div>
+</template>
+<script>
+import Changelog from "../components/about/Changelog";
+
+export default {
+  name: "home",
+  components: {
+    Changelog,
+  },
+  methods: {},
+};
+</script>
