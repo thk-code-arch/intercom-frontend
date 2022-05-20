e85512e95da5906e23d2bfc071516296b113c24e
Steffen Stein <therealstein@tutanota.com>
2022-05-20 21:12:13 +0800

    added vetur files

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,7 @@
 <!-- // Copyright (c) 2021 Steffen Stein <mail@steffenstein.com> For LICENSE see docs/LICENSE -->
 
 <template>
-  <div class="w-full px-4 px-6 sm:px-8">
+  <div class="w-full px-4 sm:px-8">
     <div class="flex p-4 flex-row w-3/4 mx-auto">
       <INTERCOM class="p-12 border-gray-400 border-r-2" />
       <CODEARCH class="p-12" />
@@ -12,11 +12,11 @@
   </div>
 </template>
 <script>
-import Changelog from "../components/about/Changelog";
-import CODEARCH from "../assets/LOGO_CODEARCH";
-import INTERCOM from "../assets/LOGO_INTERCOM";
+import Changelog from '../components/about/Changelog';
+import CODEARCH from '../assets/LOGO_CODEARCH';
+import INTERCOM from '../assets/LOGO_INTERCOM';
 export default {
-  name: "home",
+  name: 'home',
   components: {
     Changelog,
     CODEARCH,
