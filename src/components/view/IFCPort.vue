a63e75152a2cc4a3900ae56d48062f8fc84ebffb
Steffen Stein <therealstein@tutanota.com>
2022-05-30 09:12:08 +0800

    func:added IFCPort

--- src/components/view/IFCPort.vue
+++ src/components/view/IFCPort.vue
@@ -12,7 +12,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { IFCLoader } from 'three/examples/jsm/loaders/IFCLoader.js';
 import authHeader from '@/services/auth-header';
-//import projectHeader from "@/services/project-header";
+import projectHeader from '@/services/project-header';
 
 export default {
   name: 'ifc-port',
@@ -85,6 +85,8 @@ export default {
       this.dirLight.shadow.camera.far = far * 10; // default
       this.scene.add(this.dirLight);
 
+      this.loadModel();
+
       //Controls
       this.controls = new OrbitControls(this.camera, this.container);
       this.controls.keys = {
@@ -113,7 +115,6 @@ export default {
 
       // create Vector to calculate Camera Direction
       this.vector = new THREE.Vector3();
-      this.loadModel();
       this.render();
     },
 
@@ -149,81 +150,6 @@ export default {
       Array.prototype.forEach.call(rmSubproject, (sb) => {
         this.unloadSubproject(sb);
       });
-    },
-    delinit() {
-      // set container
-      this.container = this.$refs.sceneContainer;
-
-      // add camera
-      const fov = 45; // Field of view
-      const aspect = this.container.clientWidth / this.container.clientHeight;
-      const near = 0.1; // the near clipping plane
-      const far = 1000; // the far clipping plane
-      const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-      this.camera = camera;
-
-      // create scene
-      this.scene = new THREE.Scene();
-      this.scene.background = new THREE.Color('#6900eb');
-
-      // add lights
-      const ambientLight = new THREE.HemisphereLight(
-        0xffffff, // bright sky color
-        0x222222, // dim ground color
-        1 // intensity
-      );
-
-      this.scene.add(ambientLight);
-
-      this.dirLight = new THREE.DirectionalLight(0xffffff, 1.0);
-      this.dirLight.position.set(500, 500, 500);
-      this.dirLight.castShadow = true;
-      this.dirLight.shadow.mapSize.width = 2048; // default
-      this.dirLight.shadow.mapSize.height = 2048; // default
-      this.dirLight.shadow.camera = new THREE.OrthographicCamera(
-        -100,
-        100,
-        100,
-        -100,
-        0.5,
-        1000
-      );
-      this.dirLight.shadow.camera.near = near; // default
-      this.dirLight.shadow.camera.far = far * 10; // default
-      this.scene.add(this.dirLight);
-
-      // add controls
-      this.controls = new OrbitControls(this.camera, this.container);
-      //this.controls.target = new THREE.Vector3(0, 0, 0);
-      this.controls.keys = {
-        LEFT: 65, //left arrow
-        UP: 87, // up arrow
-        RIGHT: 68, // right arrow
-        BOTTOM: 83, // down arrow
-      };
-
-      // create renderer
-      this.renderer = new THREE.WebGLRenderer({ antialias: true });
-      this.renderer.setPixelRatio(window.devicePixelRatio);
-      this.renderer.outputEncoding = true;
-      this.renderer.gammaFactor = 2.2;
-      this.renderer.shadowMap.enabled = true;
-      // this.renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
-      this.container.appendChild(this.renderer.domElement);
-
-      // set aspect ratio to match the new browser window aspect ratio
-      this.camera.aspect =
-        this.container.clientWidth / this.container.clientHeight;
-      this.camera.updateProjectionMatrix();
-      this.renderer.setSize(
-        this.container.clientWidth,
-        this.container.clientHeight
-      );
-
-      // create Vector to calculate Camera Direction
-      this.vector = new THREE.Vector3();
-      // this.loadModel();
-      this.render();
     },
     loadAvatar(avatarId, name) {
       if (!this.scene.getObjectByName(name)) {
@@ -285,10 +211,43 @@ export default {
       const ifcLoader = new IFCLoader();
 
       ifcLoader.setRequestHeader({ Authorization: authHeader() });
-      ifcLoader.load('testfile.ifc', (ifc) => {
-        this.scene.add(ifc.mesh);
-        this.render();
-      });
+      ifcLoader.load(
+        `${this.$app_url}/api/project/get_projectfileifc/${projectHeader()}`,
+        (ifc) => {
+          const box = new THREE.Box3().setFromObject(ifc.mesh);
+          const size = box.getSize(new THREE.Vector3()).length();
+          const center = box.getCenter(new THREE.Vector3());
+
+          ifc.position.x += ifc.position.x - center.x;
+          ifc.position.y += ifc.position.y - center.y;
+          ifc.position.z += ifc.position.z - center.z;
+          this.camera.near = size / 100;
+          this.camera.far = size * 100;
+
+          this.camera.updateProjectionMatrix();
+
+          this.camera.position.copy(center);
+          this.camera.position.x += size / 1.0;
+          this.camera.position.y += size / 2.0;
+          this.camera.position.z += size / -1.0;
+          this.camera.lookAt(center);
+
+          this.dirLight.shadow.camera = new THREE.OrthographicCamera(
+            -size,
+            size,
+            size,
+            -size,
+            0.5,
+            1000
+          );
+
+          this.controls.maxDistance = size * 10;
+          this.controls.update();
+
+          this.scene.add(ifc.mesh);
+          this.render();
+        }
+      );
     },
 
     getCameraPosition() {
